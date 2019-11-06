aa484bf7023aa09b4177fd8bd453adf327342623
Sasha-Sokolova <mashapiterskaya@yandex.ru>
2019-11-06 15:14:06 +0300

    first homework commit

---
+++ variables/main.js
@@ -0,0 +1,24 @@
+// первая задача
+
+let algebra = 5;
+let geography = 5;
+let physics = 5;
+
+
+console.log((algebra + geography + physics) / 3);
+
+// вторая задача
+
+let myName = 'Саша';
+
+console.log("Привет, мир! Меня зовут " + myName);
+
+// третья задача
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+
+console.log(result);
