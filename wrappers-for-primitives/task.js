6ec692ed365df466d0f51169e083d297fb84bbe6
Sasha-Sokolova <mashapiterskaya@yandex.ru>
2019-11-15 15:09:56 +0300

    commit homework

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+"use strict"
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -10,10 +12,33 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    console.log(parseInt(percent));
+    console.log(parseInt(contribution));
+    console.log(parseInt(amount));
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+
+    
+    let currentDate = new Date();
+    let data = new Date(date);
+    let month = new Date((data.getFullYear() - currentDate.getFullYear()) * 12 + (data.getMonth() + 1) - (currentDate.getMonth() + 1));
+
+    let mortgage = amount - contribution;
+
+    let monthPercents = (percent / 12) / 100;
+
+
+    let monthPayment = amount * (monthPercents + monthPercents / ((Math.pow((1 + monthPercents), month) - 1)));
+
+    let totalAmount = monthPayment * month + mortgage;
+
+    console.log(totalAmount.toFixed(2));
+    return totalAmount.toFixed(2);
 }
+
+calculateTotalMortgage(10, 0, 50000, 12);
+
+
+
 
 function sayHello() {
     let name = window.personName.value;
@@ -23,6 +48,13 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    let greeting;
+    if (name.length > 0) {
+        greeting = `Привет, мир! Меня зовут ${name}`;
+    } else {
+        greeting = "Привет, мир! Меня зовут Аноним"
+    }
+
+    return greeting;
 }
+
